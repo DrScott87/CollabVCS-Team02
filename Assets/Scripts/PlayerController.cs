faed38af92a2b20087f24bd06a60ecbd675edef5
SouthpawMeerkat <Benbobagens@gmail.com>
2026-08-03 17:28:34 +1000

    Update PlayerController.cs
    Added Double jump capabilities
    Added Air Controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,13 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Movement Settings")]
     public float moveSpeed = 5f;
+    public float airControlMultiplier = 0.0f;
     public float jumpForce = 8f;
     public float gravity = -20f;
+
+    [Header("Jump Settings")]
+    public int maxJumps = 2;
+    public float doubleJumpForceMultiplier = 0.75f;
 
     [Header("Ground Detection")]
     public Transform groundCheck;
@@ -18,33 +23,41 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     private Vector3 velocity;
     private bool isGrounded;
+    private int jumpsRemaining;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        jumpsRemaining = maxJumps;
     }
 
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
         if (isGrounded && velocity.y < 0)
+        {
             velocity.y = -2f;
+            jumpsRemaining = maxJumps;
+        }
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * moveSpeed * Time.deltaTime);
+        float controlFactor = isGrounded ? 1f : airControlMultiplier;
+        controller.Move(move * moveSpeed * controlFactor * Time.deltaTime);
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
-            velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+        if (Input.GetButtonDown("Jump") && jumpsRemaining > 0)
+        {
+            float force = jumpForce;
+            if (jumpsRemaining < maxJumps) force *= doubleJumpForceMultiplier;
+            velocity.y = Mathf.Sqrt(force * -2f * gravity);
+            jumpsRemaining--;
+        }
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
 
-    public float GetCurrentSpeed()
-    {
-        return controller.velocity.magnitude;
-    }
+    public float GetCurrentSpeed() => controller.velocity.magnitude;
+    public int GetJumpsRemaining() => jumpsRemaining;
 }
